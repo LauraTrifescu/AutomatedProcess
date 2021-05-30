609fe1449a1cda46a505d13ad6d29c594c7264e2
LauraTrifescu <lauratrifescu@gmail.com>
2021-05-30 18:47:08 +0300

    Modify first table

--- 390000.py
+++ 390000.py
@@ -12,18 +12,20 @@ def add_value_for_first(file_name, value1, value2, value3, value4, value5, value
         today = date.today()
         print("Today's date:", today)
         sheet_to_focus = '390000'
-
+        
         for s in range(len(wb.sheetnames)):
             if wb.sheetnames[s] == sheet_to_focus:
                 break
+
         wb.active = s
         ws = wb.active
-        tb = ws.tables["Table3"]
+        tb = ws.tables["Table2"]
+        # ws.append([today,value1])
         coord = list(range_boundaries(tb.ref))
 
         data = [[today, value1, value2, value3, value4, value5, value6, value7, value8, value9, value10, value11, value12, value13, value14, value15, value16, value17, value18, value19, value20, value21, value22, value23, value24, value25, value26, value27, value28, value29, value30, value31, value32, value33, value34, value35, value36, value37, value38, value39]]
         for row in data:
-            coord[-1] += 1 
+            coord[-1] += 1  # <= No need to use max_row, just add one line!
             ws.append(row)
 
         tb.ref = f"{get_column_letter(coord[0])}{coord[1]}:{get_column_letter(coord[2])}{coord[3]}"
